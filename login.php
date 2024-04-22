79734011a84f2a36d74f37ab08f06d259135fe0d
Valeria <valeriacasquero@gmail.com>
2024-04-22 19:59:23 -0300

    validacion-de-user-pass

--- login.php
+++ login.php
@@ -1,44 +1,76 @@
 <?php
 
-echo "<pre>";( "pre" muestra datos preformateados)
+
 var_dump($_POST);
-echo ""
+
+$usuarioPrueba_user = 'usuario@prueba.ts';
+$usuarioPrueba_pass = 'password';
+$usuarioPrueba_passHash = password_hash($usuarioPrueba_pass, PASSWORD_DEFAULT);
+
+echo 'usuario prueba; '.$usuarioPrueba_user.'<br>';
+echo 'contraseña prueba: '.$usuarioPrueba_pass.'<br>';
+echo 'contraseña hasheada: '.$usuarioPrueba_passHash.'<br>';
+
+$email = '';
+$password = '';
+
+$error_mail= '';
+$error_pass= '';
 
 if (isset($_POST['send'])) {
 
     if (!isset($_POST['email'])){      //!isset busca si no esta en el array
         //esta seteado el email
         echo 'No existe email';
-    }else {
+    }else{
         echo 'Si existe el email';
-        $email = $_POST['email'];
-    
-        if (empty(trim$email)){                 //trim quita los vacios antes y despues del string
-            echo 'email no puede estar vacio'
-        }
-        else{
-            echo 'email no esta vacio'
-        }
-            if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            echo "el email es correcto");
-            } else {
-            echo"El email es incorrecto");
+        $email = trim($_POST['email']);//trim quita los vacios antes y despues del string
+        
+        if (empty($email)){
+                echo 'email no puede estar vacio';
+        }else{
+            echo 'Email no esta vacio';
+            if(strlen($email)<5 || strlen($email)>120){                //strlen es breviatura de string leng (largo del string) y te devuelve un estero (cantidad de caracteres dentro de la variable)
+                echo 'Ingrese un correo entre 5 y 120 casactéres';
+            }else{
+                if (filter_var($email, FILTER_VALIDATE_EMAIL)){
+                    echo "el email es correcto";
+                }else{
+                    echo"El email es incorrecto";
+                }
             }
-    }
-    
-    if (!isset($_POST['password'])){
-        echo 'la contraseña no existe';
-    }else {
-        echo 'existe la contraseña';
-        $password = $_POST['password'];
-    
-        if (empty(trim$password)){
-            echo 'la contraseña no puede estar vacia';
-        }else {
-            echo 'la contraseña no esta vacia';
         }
-    
-}
+    }
+
+    if (!isset($_POST['password'])){      //!isset busca si no esta en el array
+        //esta seteado el email
+        echo 'No existe la contraseña';
+    }else{
+        echo 'Si existe la contraseña';
+        $password = trim($_POST['password']);//trim quita los vacios antes y despues del string
+        if (empty($password)){
+                echo 'la contraseña no puede estar vacio';
+        }else {
+            echo 'la contraseña no esta vacio';
+            if(strlen($password)<3 || strlen($password)>10){                //strlen es breviatura de string leng (largo del string) y te devuelve un estero (cantidad de caracteres dentro de la variable)
+                echo 'Ingrese un correo entre 3 y 10 casactéres';
+            }else{
+                echo"La cantidad de caractéres es correcta";
+                if ($email === $usuarioPrueba_user) {
+                    $verificar = password_verify($password, $usuarioPruebaHash);
+                    if ($verificar === false) {
+                        echo 'contraseña incorrecta';
+                    }else{
+                        echo 'todo correcto.<hr>';
+                    }
+                }else{
+                    echo 'usuario incorrecto.<br>';
+                }
+            }
+        }
+    }
+} 
+?>
 
 
 
@@ -57,9 +89,10 @@ if (isset($_POST['send'])) {
     <main class="row flex flex-justify-center login_cont">
         <form class="col_4 login_form" method="post">
             <div class="col_12 title_login"> <h1>INICIAR SESION</h1></div>
-            <div class="col_12 mail_login"> <input type="email" placeholder="ingrese su email" required></div>
-            <div class="col_12 pass_login"> <input type="password" placeholder="ingrese su contraseña" required></div>
-            <div class="col_12 button_login"> <button>Iniciar</button></div>
+            <div class="col_12 mail_login"> <input type="email" name="email" placeholder="ingrese su email" ></div>
+            <div class="col_12 pass_login"> <input type="password" name="password
+            " placeholder="ingrese su contraseña" ></div>
+            <div class="col_12 button_login"> <button type="submit" name="send">Iniciar</button></div>
         </form>
     </main>
     
